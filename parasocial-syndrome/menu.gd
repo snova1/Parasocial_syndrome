ff3bd38ef249db1af6c33d9f81a49822746d8254
snova1 <snova@unal.edu.co>
2025-02-09 10:50:08 -0500

    Cambios a menú

--- parasocial-syndrome/menu.gd
+++ parasocial-syndrome/menu.gd
@@ -1,5 +1,45 @@
 extends Control
 
+var buttons = []
+var selected_index = -1
+var hovered_index= -1
+@onready var v_box_container: VBoxContainer = $MarginContainer/HBoxContainer/VBoxContainer
+
+func _ready():
+	buttons = v_box_container.get_children().filter(func(child): return child is Button)
+	for i in range(buttons.size()):
+		buttons[i].connect("mouse_entered",Callable(self, "_on_button_hovered").bind(i))
+		buttons[i].connect("mouse_exited",Callable(self, "_on_button_unhovered").bind(i))
+		buttons[i].connect("focus_exited",Callable(self, "_on_button_unhovered").bind(i))
+	update_selection()
+
+func update_selection():
+	for i in range(buttons.size()):
+		if i == selected_index or i == hovered_index:
+			buttons[i].text = "> " + buttons[i].text.lstrip(">")
+		else:
+			buttons[i].text = buttons[i].text.lstrip(">")
+
+func _input(event):
+	if event.is_action_pressed("ui_down"):
+		selected_index = (selected_index + 1) % buttons.size()
+		hovered_index= -1
+		update_selection()
+	elif event.is_action_pressed("ui_up"):
+		selected_index = (selected_index - 1 + buttons.size()) % buttons.size()
+		hovered_index= -1
+		update_selection()
+
+func _on_button_hovered(index):
+	hovered_index= index
+	selected_index = -1
+	update_selection()
+
+func _on_button_unhovered(index):
+	if hovered_index == index:
+		hovered_index=-1
+	update_selection()
+
 func _on_iniciar_pressed() -> void:
 	get_tree().change_scene_to_file("res://initial.tscn")
 
