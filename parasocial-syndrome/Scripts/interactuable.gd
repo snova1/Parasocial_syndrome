be6883ec0b63549b5064f29930edea6eaa520112
5xf9 <dani5xf@gmail.com>
2025-11-30 15:28:37 -0500

    Update interactuable.gd

--- parasocial-syndrome/Scripts/interactuable.gd
+++ parasocial-syndrome/Scripts/interactuable.gd
@@ -9,3 +9,6 @@ func action() ->void:
 	var balloon: Node= Balloon.instantiate()
 	get_tree().current_scene.add_child(balloon)
 	balloon.start(dialogue_resource,dialogue_start)
+
+	if (get_parent().has_method("grab")):
+		get_parent().grab()
