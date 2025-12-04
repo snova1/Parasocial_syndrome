e540a5cd9ba07daf88f6b2a8e40de47a78540242
5xf9 <dani5xf@gmail.com>
2025-12-04 17:03:16 -0500

    Update cocina.gd

--- parasocial-syndrome/Scripts/Rooms/cocina.gd
+++ parasocial-syndrome/Scripts/Rooms/cocina.gd
@@ -1,4 +1,18 @@
 extends "res://Scripts/Rooms/BaseNivel.gd"
 
+@onready var antagonista: CharacterBody2D = $Antagonista
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+signal finished
+
 func _ready() -> void:
 	super()
+	antagonista.visible=false
+	antagonista._disable_collision()
+	Status.cutscene.connect(_on_cutscene)
+	
+func _on_cutscene(name: String):
+	animation_player.play(name)
+	await animation_player.animation_finished
+	await get_tree().create_timer(0.5).timeout
+	finished.emit()
