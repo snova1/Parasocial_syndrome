e1cc75dfe43ef5f4fe1a6783d9ed15f2037c21a8
snova1 <snova@unal.edu.co>
2025-12-03 19:37:34 -0500

    Added logic of puzzle in wait for assets plus Kiran mechanic

--- parasocial-syndrome/Scripts/Rooms/sala_estar.gd
+++ parasocial-syndrome/Scripts/Rooms/sala_estar.gd
@@ -3,5 +3,10 @@ extends "res://Scripts/Rooms/BaseNivel.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	puerta_e.get_node("CollisionShape2D").disabled =true
+	if Puzzle.door_garden=="closed":
+		puerta_e.get_node("CollisionShape2D").disabled =true
+	Puzzle.open_door.connect(_on_open_door)
 	super()
+
+func _on_open_door():
+	puerta_e.get_node("CollisionShape2D").disabled =false
